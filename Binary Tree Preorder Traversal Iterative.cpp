30ab77bacbb62879cb6c1e3c2c40825cee826310
Sahil Raj <sahilraj7094@gmail.com>
2025-07-20 05:57:36 +0530

    Create Binary Tree Preorder Traversal Iterative.cpp

---
+++ Binary Tree Preorder Traversal Iterative.cpp
@@ -0,0 +1,37 @@
+/*
+Given the root of a binary tree, return the preorder traversal of its nodes' values.
+Example 1:
+Input: root = [1,null,2,3]
+Output: [1,2,3]
+Example 2:
+Input: root = [1,2,3,4,5,null,8,null,null,6,7,9]
+Output: [1,2,4,5,6,7,3,8,9]
+Example 3:
+Input: root = []
+Output: []
+Example 4:
+Input: root = [1]
+Output: [1]
+Code :-
+*/
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> preorder;
+        if(root==NULL) return preorder;
+        stack<TreeNode*> st;
+        st.push(root);
+        while(!st.empty()){
+            root=st.top();
+            st.pop();
+            preorder.push_back(root->val);
+            if(root->right!=NULL){
+                st.push(root->right);
+            }
+            if(root->left!=NULL){
+                st.push(root->left);
+            }
+        }
+        return preorder;
+    }
+};
